af0a072082cb4021179c6b0f15faa772429ae085
costcon <tentekomambo@gmail.com>
2021-04-20 10:54:00 +0000

    Finish 4_20

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,12 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
-  validates :name, length: { in: 2..20 }
+  validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
   def posts
   return Post.where(user_id: book.id)
   end
+
 
 end
